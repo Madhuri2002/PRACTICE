1c74db03a213e74d00109af786121265737fa8c7
Madhuri Banuri <76729370+Madhuri2002@users.noreply.github.com>
2022-11-23 18:06:20 +0530

    Update tower.cs

--- tower.cs
+++ tower.cs
@@ -1,18 +1,19 @@
 using System;
 namespace height
 {
-class tower
-{
-public static void main(String[] arg){
-int n=Convert.ToInt32(Console.ReadLine());
-int k=Convert.ToInt32(Console.ReadLine());
-int[] a=new int[n];
-for(int i=0;i<n;i++){
-a[i]=Convert.ToInt32(Console.ReadLine());
-}
-Array.Sort(a);
-int min=a[0]+k;
-int max=a[n-1]-k;
-Console.WriteLine(max-min);
-}
+  class tower
+  {
+    public static void main(String[] arg){
+    int n=Convert.ToInt32(Console.ReadLine());
+    int k=Convert.ToInt32(Console.ReadLine());
+    int[] a=new int[n];
+    for(int i=0;i<n;i++)
+    {
+      a[i]=Convert.ToInt32(Console.ReadLine());
+    }
+      Array.Sort(a);
+      int min=a[0]+k;
+      int max=a[n-1]-k;
+      Console.WriteLine(max-min);
+   }
 }
